d0d0dc9d93d28b1fb6a89f44e80dd2db00b51d14
gabdb <gabrielnyssens@gmail.com>
2025-01-30 15:51:14 +0100

    debut parsing

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 16:52:10 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/01/29 15:28:50 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:27:45 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@
 
 int main(void)
 {
+	int		fd;
+	
+	fd = open("map.cub", O_RDONLY);
+	if (-1 == fd)
+		return(write(2, "error opening file\n", 19), 0);
 	
 }
--- source/parsing.c
+++ source/parsing.c
@@ -6,20 +6,52 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:52:31 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/01/29 15:38:58 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:51:03 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-char	**parsing(void)
+int	only_ones(char *str)
 {
-	int	fd;
-	int	row_len;
-	int	col_len;
+	int	i;
 
-	fd = open("map.cub", O_RDONLY);
-	if (-1 == fd)
-		return(write(2, "error opening file\n", 19), NULL);
-	
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != '1')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char	**parsing(int fd)
+{
+	int		i;
+	int		row_len;
+	int		col_len;
+	char	*line;
+	char	**result;
+
+	line = NULL;
+	i = 0;
+	while (line != NULL || i == 0)
+	{
+		if (line)
+			free(line);
+		line = get_next_line(fd);
+		if (0 == i)
+		{
+			row_len = ft_strlen(line);
+			if (!only_ones(line))
+				return(write(2, "invalid map\n", 12), NULL);
+		}
+		else 
+		{
+			if (line[0] != '1' || line [ft_strlen(line) - 1] != '1')
+				return(write(2, "invalid map\n", 12), NULL);
+		}
+		
+	}
 }
