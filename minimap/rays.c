b7d068c484d7910593f1959b2c569eeb3ad133dc
Gabriel Nyssens <gnyssens@fu-r1-p3.s19.be>
2025-03-03 16:42:47 +0100

    doubler check_wall, un pr rays sans check arriere qui creait un bug daffichage

--- minimap/rays.c
+++ minimap/rays.c
@@ -6,11 +6,62 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 23:03:39 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/03/03 15:36:11 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/03/03 16:41:37 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+int	rays_check_wall(t_mlx *data, int x, int y)
+{
+	int			old_x;
+	int			old_y;
+	int			dx; //dir_x
+	int			dy; //dir_y
+
+	if (x < 0 || y < 0 || x > data->num_rows || y > data->num_rows)
+		return (0);
+	if (data->map[y][x] == '1')
+		return (0);
+	old_x = (int)data->player->x_pos;
+	old_y = (int)data->player->y_pos;
+	if (data->player->dir_x > 0)
+		dx = 1;
+	else if (data->player->dir_x == 0)
+		dx = 0;
+	else
+		dx = -1;
+	if (data->player->dir_y > 0)
+		dy = 1;
+	else if (data->player->dir_y == 0)
+		dy = 0;
+	else
+		dy = -1;
+	if (old_x != x && old_y != y) //ca veut dire que ya changement diagonal (de 2 cases)
+	{
+		if (dx == 1 && dy == 1)
+		{
+			if (data->map[y - 1][x] == '1' && data->map[y][x - 1] == '1')
+				return (0);
+		}
+		else if (dx == 1 && dy == -1)
+		{
+			if (data->map[y + 1][x] == '1' && data->map[y][x - 1] == '1')
+				return (0);
+		}
+		else if (dx == -1 && dy == 1)
+		{
+			if (data->map[y - 1][x] == '1' && data->map[y][x + 1] == '1')
+				return (0);
+		}
+		else if (dx == -1 && dy == -1)
+		{
+			if (data->map[y + 1][x] == '1' && data->map[y][x + 1] == '1')
+				return (0);
+		}
+	}
+	return (1);
+}
 
 // chatGPT (pcq flemme)
 void draw_long_line(t_mlx *data, double x, double y, double dir_x, double dir_y)
@@ -25,7 +76,7 @@ void draw_long_line(t_mlx *data, double x, double y, double dir_x, double dir_y)
 	i = 0;
     while (1)
 	{
-		if (!check_wall(data,(img_x / cellsize), (img_y - WINDOW_HEIGHT + (data->num_rows * cellsize)) / cellsize))
+		if (!rays_check_wall(data,(img_x / cellsize), (img_y - WINDOW_HEIGHT + (data->num_rows * cellsize)) / cellsize))
 			break ;
 		if (i == 0 || i % cellsize == 0)
         	my_mlx_pixel_put(data, (int) img_x, (int) img_y, BLUE);
