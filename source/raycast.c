91b59dffb9c637e4f9353193a3de5b45d743b047
Gabriel Nyssens <gnyssens@fu-c2-p1.s19.be>
2025-03-19 18:11:55 +0100

    norme OK raycast.c

--- source/raycast.c
+++ source/raycast.c
@@ -6,18 +6,19 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 00:45:32 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/03/19 17:55:05 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/03/19 18:11:42 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-int get_sprite_pixel(t_img *sprite, int x, int y)
+int	get_sprite_pixel(t_img *sprite, int x, int y)
 {
-    char *pixel = sprite->addr
-                + (y * sprite->line_length)
-                + (x * (sprite->bpp / 8));
-    return *(int *)pixel;
+	char	*pixel;
+
+	pixel = sprite->addr + (y * sprite->line_length)
+		+ (x * (sprite->bpp / 8));
+	return (*(int *)pixel);
 }
 
 void	init_variables(t_raycast *r, t_mlx *data, int x)
@@ -47,17 +48,20 @@ void	initial_distance(t_mlx *data, t_raycast *r)
 	else
 	{
 		r->step_x = 1;
-		r->side_dist_x = ((r->map_x + 1) - data->player->x_pos) * r->delta_dist_x;
+		r->side_dist_x = ((r->map_x + 1)
+				- data->player->x_pos) * r->delta_dist_x;
 	}
 	if (r->raydir_y < 0)
 	{
 		r->step_y = -1;
-		r->side_dist_y = (data->player->y_pos - r->map_y) * r->delta_dist_y;
+		r->side_dist_y = (data->player->y_pos
+				- r->map_y) * r->delta_dist_y;
 	}
 	else
 	{
 		r->step_y = 1;
-		r->side_dist_y = ((r->map_y + 1) - data->player->y_pos) * r->delta_dist_y;
+		r->side_dist_y = ((r->map_y + 1)
+				- data->player->y_pos) * r->delta_dist_y;
 	}
 	r->hit = 0;
 }
@@ -89,7 +93,7 @@ void	actual_dda(t_mlx *data, t_raycast *r)
 		r->perp_wall_dis = r->side_dist_y - r->delta_dist_y;
 }
 
-void render_3d(t_mlx *data)
+void	render_3d(t_mlx *data)
 {
 	t_raycast	r;
 	int			x;
@@ -101,49 +105,17 @@ void render_3d(t_mlx *data)
 		initial_distance(data, &r);
 		actual_dda(data, &r);
 		calc_wall_height(&r);
-        // 8. Draw the vertical line for this column
-		if (r.side == 0) //vertical wall
-		{
-			r.wall_x = data->player->y_pos + r.perp_wall_dis * r.raydir_y;
-			if (r.raydir_x < 0) // west
-				r.index_texture = 2;
-			else
-				r.index_texture = 3; //east
-		}
-		else //horizontal wall
-		{
-			r.wall_x = data->player->x_pos + r.perp_wall_dis * r.raydir_x;
-			if (r.raydir_y < 0)
-				r.index_texture = 0; //north
-			else
-				r.index_texture = 1; //south
-		}
-		r.wall_x -= floor(r.wall_x); // keep fractional part
-		r.tex_width = data->textures[r.index_texture].width;
-		r.tex_height = data->textures[r.index_texture].height;
-		r.tex_x = (int)(r.wall_x * (double)r.tex_width);
-		if (r.side == 0 && r.raydir_x > 0)
-			r.tex_x = r.tex_width - r.tex_x - 1;
-		if (r.side == 1 && r.raydir_y < 0)
-			r.tex_x = r.tex_width - r.tex_x - 1;
-		// [TEXTURE CODE HERE] Now draw the column using the texture
+		prep_vertical_line(data, &r);
 		r.step = 1.0 * r.tex_height / r.line_height;
-		r.tex_pos = (r.draw_start - WINDOW_HEIGHT / 2 + r.line_height / 2) * r.step;
-		r.z = -1; //draw ceiling
+		r.tex_pos = (r.draw_start - WINDOW_HEIGHT / 2
+				+ r.line_height / 2) * r.step;
+		r.z = -1;
 		while (++r.z < r.draw_start)
 			my_mlx_pixel_put(data, x, r.z, data->ceiling_color);
-		for (int y = r.draw_start; y < r.draw_end; y++)
-		{
-			r.tex_y = (int)r.tex_pos & (r.tex_height - 1);
-			r.tex_pos += r.step;
-			r.color = get_texture_pixel(&data->textures[r.index_texture], r.tex_x, r.tex_y);
-			my_mlx_pixel_put(data, x, y, r.color);
-			r.z = y;
-		}
-		while (++r.z < WINDOW_HEIGHT) //draw_floor
+		draw_vertical_line(data, &r, x);
+		while (++r.z < WINDOW_HEIGHT)
 			my_mlx_pixel_put(data, x, r.z, data->floor_color);
-    }
-    mlx_put_image_to_window(data->mlx, data->window, data->image, 0, 0);
+	}
+	mlx_put_image_to_window(data->mlx, data->window, data->image, 0, 0);
 	render(data);
 }
-
--- source/raycast_norm.c
+++ source/raycast_norm.c
@@ -6,7 +6,7 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:51:06 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/03/19 17:55:09 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/03/19 18:06:04 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	calc_wall_height(t_raycast *r)
 {
-	// 7. Calculate wall height and draw start/end points
 	r->line_height = (int)(WINDOW_HEIGHT / r->perp_wall_dis);
 	r->draw_start = -r->line_height / 2 + WINDOW_HEIGHT / 2;
 	if (r->draw_start < 0)
@@ -22,4 +21,48 @@ void	calc_wall_height(t_raycast *r)
 	r->draw_end = r->line_height / 2 + WINDOW_HEIGHT / 2;
 	if (r->draw_end >= WINDOW_HEIGHT)
 		r->draw_end = WINDOW_HEIGHT - 1;
+}
+
+void	prep_vertical_line(t_mlx *data, t_raycast *r)
+{
+	if (r->side == 0)
+	{
+		r->wall_x = data->player->y_pos + r->perp_wall_dis * r->raydir_y;
+		if (r->raydir_x < 0)
+			r->index_texture = 2;
+		else
+			r->index_texture = 3;
+	}
+	else
+	{
+		r->wall_x = data->player->x_pos + r->perp_wall_dis * r->raydir_x;
+		if (r->raydir_y < 0)
+			r->index_texture = 0;
+		else
+			r->index_texture = 1;
+	}
+	r->wall_x -= floor(r->wall_x);
+	r->tex_width = data->textures[r->index_texture].width;
+	r->tex_height = data->textures[r->index_texture].height;
+	r->tex_x = (int)(r->wall_x * (double)r->tex_width);
+	if (r->side == 0 && r->raydir_x > 0)
+		r->tex_x = r->tex_width - r->tex_x - 1;
+	if (r->side == 1 && r->raydir_y < 0)
+		r->tex_x = r->tex_width - r->tex_x - 1;
+}
+
+void	draw_vertical_line(t_mlx *data, t_raycast *r, int x)
+{
+	int	y;
+
+	y = r->draw_start - 1;
+	
+	while (++y < r->draw_end)
+	{
+		r->tex_y = (int)r->tex_pos & (r->tex_height - 1);
+		r->tex_pos += r->step;
+		r->color = get_texture_pixel(&data->textures[r->index_texture], r->tex_x, r->tex_y);
+		my_mlx_pixel_put(data, x, y, r->color);
+		r->z = y;
+	}
 }
