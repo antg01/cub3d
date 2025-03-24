e478a5d2d1d00577aa863558dac599af694d767f
Gabriel Nyssens <gnyssens@fu-r8-p6.s19.be>
2025-03-24 16:25:47 +0100

    norme finie

--- source/textures.c
+++ source/textures.c
@@ -5,54 +5,66 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/20 19:05:39 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/03/24 15:51:04 by gnyssens         ###   ########.fr       */
+/*   Created: 2025/03/24 16:25:21 by gnyssens          #+#    #+#             */
+/*   Updated: 2025/03/24 16:25:23 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-t_img	*init_textures(void)
+void	load_text1(t_mlx *data, int *height, int *width, int i)
 {
-	t_img	*result;
+	data->textures[i].img_ptr = mlx_xpm_file_to_image(data->mlx,
+			"textures/redbrick.xpm", width, height);
+	data->textures[i].height = *height;
+	data->textures[i].width = *width;
+	data->textures[i].addr = mlx_get_data_addr(data->textures[i].img_ptr,
+			&data->textures[i].bpp, &data->textures[i].line_length,
+			&data->textures[i].endian);
+	data->textures[i].img_ptr = mlx_xpm_file_to_image(data->mlx,
+			"textures/bluestone.xpm", width, height);
+	data->textures[i].height = *height;
+	data->textures[i].width = *width;
+	data->textures[i].addr = mlx_get_data_addr(data->textures[i].img_ptr,
+			&data->textures[i].bpp, &data->textures[i].line_length,
+			&data->textures[i].endian);
+}
 
-	result = safe_malloc(sizeof(t_img) * 4);
-	return (result);
+void	load_text2(t_mlx *data, int *height, int *width, int i)
+{
+	data->textures[i].img_ptr = mlx_xpm_file_to_image(data->mlx,
+			"textures/eagle.xpm", width, height);
+	data->textures[i].height = *height;
+	data->textures[i].width = *width;
+	data->textures[i].addr = mlx_get_data_addr(data->textures[i].img_ptr,
+			&data->textures[i].bpp, &data->textures[i].line_length,
+			&data->textures[i].endian);
+	data->textures[i].img_ptr = mlx_xpm_file_to_image(data->mlx,
+			"textures/greystone.xpm", width, height);
+	data->textures[i].height = *height;
+	data->textures[i].width = *width;
+	data->textures[i].addr = mlx_get_data_addr(data->textures[i].img_ptr,
+			&data->textures[i].bpp, &data->textures[i].line_length,
+			&data->textures[i].endian);
 }
 
 void	load_textures(t_mlx *data)
 {
 	int	height;
 	int	width;
+	int	i;
 
-	data->textures[0].img_ptr = mlx_xpm_file_to_image(data->mlx,
-			"textures/redbrick.xpm", &width, &height);
-	data->textures[0].height = height;
-	data->textures[0].width = width;
-	data->textures[0].addr = mlx_get_data_addr(data->textures[0].img_ptr,
-			&data->textures[0].bpp, &data->textures[0].line_length,
-			&data->textures[0].endian);
-	data->textures[1].img_ptr = mlx_xpm_file_to_image(data->mlx,
-			"textures/bluestone.xpm", &width, &height);
-	data->textures[1].height = height;
-	data->textures[1].width = width;
-	data->textures[1].addr = mlx_get_data_addr(data->textures[1].img_ptr,
-			&data->textures[1].bpp, &data->textures[1].line_length,
-			&data->textures[1].endian);
-	data->textures[2].img_ptr = mlx_xpm_file_to_image(data->mlx,
-			"textures/eagle.xpm", &width, &height);
-	data->textures[2].height = height;
-	data->textures[2].width = width;
-	data->textures[2].addr = mlx_get_data_addr(data->textures[2].img_ptr,
-			&data->textures[2].bpp, &data->textures[2].line_length,
-			&data->textures[2].endian);
-	data->textures[3].img_ptr = mlx_xpm_file_to_image(data->mlx,
-			"textures/greystone.xpm", &width, &height);
-	data->textures[3].height = height;
-	data->textures[3].width = width;
-	data->textures[3].addr = mlx_get_data_addr(data->textures[3].img_ptr,
-			&data->textures[3].bpp, &data->textures[3].line_length,
-			&data->textures[3].endian);
+	i = 0;
+	while (i < 2)
+	{
+		load_text1(data, &height, &width, i);
+		i++;
+	}
+	while (i < 4)
+	{
+		load_text2(data, &height, &width, i);
+		i++;
+	}
 }
 
 int	get_texture_pixel(t_img *texture, int x, int y)
